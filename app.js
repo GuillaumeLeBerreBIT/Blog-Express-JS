f6888f60fb5451201453fd056e07a4851dcc4cb5
GuillaumeLeBerreBIT <leberreguillaume.glb@gmail.com>
2025-07-20 22:42:37 +0200

    Add all routings for handlign the forms and showing blog data

---
+++ app.js
@@ -0,0 +1,74 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+const blogPostsMem = [];
+
+//Middleware
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+//Routes
+app.get("/", (req, res) => {
+
+  res.render("index.ejs", { blogPostsMem: blogPostsMem });
+});
+
+app.get("/create", (req, res) => {
+  res.render("uploadPost.ejs");
+});
+
+app.post("/upload", (req, res) => {
+    console.log("Processing form");
+    let blogPost;
+
+    let blogMatch = blogPostsMem.filter(b => String(b.id) === req.body['idBlog']);
+    console.log(blogMatch);
+    if (blogMatch.length === 0) {
+
+        blogPost = {
+            title: req.body["title"],
+            text: req.body["story"],
+            id: new Date().getTime(),
+        };
+
+        blogPostsMem.push(blogPost);
+
+    } else {
+
+        blogPost = {
+            title: req.body["title"],
+            text: req.body["story"],
+            id: req.body['idBlog']
+        };
+
+        let indexMatch = blogPostsMem.findIndex(b => String(b.id) === req.body['idBlog']);
+        blogPostsMem[indexMatch] = blogPost;
+    };
+
+    res.redirect("/"); // Go to the home page showing the Story added on top.
+});
+
+app.get("/edit/:id", (req, res) => {
+  let idBlog = req.params.id;
+
+  let postEdit = blogPostsMem.filter((e) => {
+    return String(e.id)=== String(idBlog);
+  });
+
+  res.render("uploadPost.ejs", {blog: postEdit[0]});
+});
+
+app.post('/delete/:id', (req, res) => {
+    let idBlog = req.params.id;
+
+    let indexMatch = blogPostsMem.findIndex(b => String(b.id) === idBlog);
+
+    blogPostsMem.splice(indexMatch, 1);   
+
+    res.render('index.ejs', {blogPostsMem: blogPostsMem});
+})
+
+app.listen(port, () => {
+  console.log(`The app is being served at port ${port}`);
+});
